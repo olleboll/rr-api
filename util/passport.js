b31eef6f1a092a47fe5502d631cb3defd99aa425
Olle Wahlström <owlen88@gmail.com>
2018-12-16 17:16:35 +0100

    adds osm logging

--- util/passport.js
+++ util/passport.js
@@ -5,6 +5,9 @@ passport.use(new HeaderAPIKeyStrategy(
   { header: 'Authorization', prefix: 'Api-Key ' },
   false,
   (apikey, done) => {
+    console.log("A request has been made!")
+    console.log(apikey)
+    console.log(process.env.API_KEY)
     if (apikey === process.env.API_KEY) {
       return done(null, { authenticated: true })
     }
